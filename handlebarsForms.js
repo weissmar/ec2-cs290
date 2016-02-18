6879cd4fcc450f328c41332313494ad6afda0650
weissmar <weissmar@oregonstate.edu>
2016-02-17 17:12:03 -0800

    adds 404 and 500 pages

--- handlebarsForms.js
+++ handlebarsForms.js
@@ -37,6 +37,17 @@ app.post('/form-results', function(req,res){
 	res.render('form-results', context);
 });
 
+app.use(function(req,res){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err,req,res,next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
 app.listen(app.get('port'), function(){
 	console.log('Express started on http:localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
