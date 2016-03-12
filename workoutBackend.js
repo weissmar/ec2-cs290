8703e37543323b24f4b3177b856386f54a4a513c
weissmar <weissmar@oregonstate.edu>
2016-03-12 12:30:15 -0800

    more workout app stuff

--- workoutBackend.js
+++ workoutBackend.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 var mysql = require('mysql');
 var pool = mysql.createPool({
@@ -13,11 +14,14 @@ var pool = mysql.createPool({
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 app.post('/addExercise', function(req,res,next){
 	pool.query("INSERT INTO workouts (name, reps, weight, date, lbs) VALUES (?, ?, ?, ?, ?)", 
-		[req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+		[req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], 
+		function(err, result){
 		if(err){
 			next(err);
 			return;
@@ -31,6 +35,64 @@ app.post('/addExercise', function(req,res,next){
 			res.send(JSON.stringify(rows[0]));
 		});
 	});
+});
+
+app.post('/updateExercise', function(req,res,next){
+	pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?", 
+		[req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs, req.query.id], 
+		function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, rows, fields){
+			if(err){
+				next(err);
+				return;
+			}
+			res.type('text/plain');
+			res.send(JSON.stringify(rows[0]));
+		});
+	});
+});
+
+app.post('/deleteExercise', function(req,res,next){
+	pool.query("DELETE FROM workouts WHERE id=?", [req.query.id], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		res.type('text/plain');
+		res.send(JSON.stringify(result));
+	});
+});
+
+app.get('/getTable', function(req,res,next){
+	pool.query("SELECT * FROM workouts", function(err, rows, fields){
+		if(err){
+			next(err);
+			return;
+		}
+		res.type('text/plain');
+		res.send(JSON.stringify(rows));
+	});
+});
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ 
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('resetTable',context);
+    });
+  });
 });
 
 app.use(function(req,res){
